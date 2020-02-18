5e2345399c95ac291771861daf964bb7ac19d4c4
dyaroshevych <dyaroshevych@gmail.com>
2020-02-18 23:33:21 +0300

    Updates

---
+++ precache-manifest.226bd6522fc184d11d8afe4678b86205.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a5ff0c16da5cce7803597f72409e3c",
+    "url": "/advanced-hemp-trailer/index.html"
+  },
+  {
+    "revision": "6a07c2427d110441e297",
+    "url": "/advanced-hemp-trailer/static/css/main.02b2c144.chunk.css"
+  },
+  {
+    "revision": "5850129b2cd32a57a70c",
+    "url": "/advanced-hemp-trailer/static/js/2.e75b795c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/advanced-hemp-trailer/static/js/2.e75b795c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a07c2427d110441e297",
+    "url": "/advanced-hemp-trailer/static/js/main.91d25fd5.chunk.js"
+  },
+  {
+    "revision": "366aa02e508b8bf898d4",
+    "url": "/advanced-hemp-trailer/static/js/runtime-main.7eb5c5b6.js"
+  },
+  {
+    "revision": "f4dd5eb1a9a7a8ad9c9b307656d7a33a",
+    "url": "/advanced-hemp-trailer/static/media/about-1.f4dd5eb1.jpg"
+  },
+  {
+    "revision": "2f09e57c0794c88579a28511e41dfe5c",
+    "url": "/advanced-hemp-trailer/static/media/about-2.2f09e57c.jpg"
+  },
+  {
+    "revision": "e6d20b9f6cdf9f92d72949ca28ac0cac",
+    "url": "/advanced-hemp-trailer/static/media/benefits-1.e6d20b9f.svg"
+  },
+  {
+    "revision": "a9c15641bdc8f39b2e206f3beae45273",
+    "url": "/advanced-hemp-trailer/static/media/benefits-2.a9c15641.svg"
+  },
+  {
+    "revision": "e6debeb2f4cfc09d7af8302e3ae320a1",
+    "url": "/advanced-hemp-trailer/static/media/benefits-3.e6debeb2.svg"
+  },
+  {
+    "revision": "28376719ab8ae7e9be1ebcc0db613dfe",
+    "url": "/advanced-hemp-trailer/static/media/benefits-4.28376719.svg"
+  },
+  {
+    "revision": "a83899f72f1f6da5206d7ffe6d43a600",
+    "url": "/advanced-hemp-trailer/static/media/header-bg.a83899f7.jpg"
+  },
+  {
+    "revision": "c9865e20890b57617e844c57d881a5d2",
+    "url": "/advanced-hemp-trailer/static/media/how-it-works-1.c9865e20.svg"
+  },
+  {
+    "revision": "99702617406717566875579ef04d0b2c",
+    "url": "/advanced-hemp-trailer/static/media/how-it-works-2.99702617.svg"
+  },
+  {
+    "revision": "dc80c994a9fbf237e7519e00e92db324",
+    "url": "/advanced-hemp-trailer/static/media/how-it-works-3.dc80c994.svg"
+  },
+  {
+    "revision": "306363a1f779c7c84198f84cd11aaa8e",
+    "url": "/advanced-hemp-trailer/static/media/product-preview-1.306363a1.jpg"
+  },
+  {
+    "revision": "f76e43764b20e8614a222a0785176d77",
+    "url": "/advanced-hemp-trailer/static/media/product-preview-2.f76e4376.jpg"
+  },
+  {
+    "revision": "17c4e03f1eebc5cd76b093d272a77202",
+    "url": "/advanced-hemp-trailer/static/media/product-preview-3.17c4e03f.jpg"
+  },
+  {
+    "revision": "692cd88c38a46fad430368593d392a51",
+    "url": "/advanced-hemp-trailer/static/media/support-bg.692cd88c.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/advanced-hemp-trailer/precache-manifest.226bd6522fc184d11d8afe4678b86205.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/advanced-hemp-trailer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
